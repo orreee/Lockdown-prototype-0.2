3c7354bcde31947b6da164c2f823e1fd5d3f9571
orreee <oscar.sandahl@gmail.com>
2023-04-18 00:38:54 +0200

    fixed some more things

--- Assets/Lucas Assets/Scripts/Player.cs
+++ Assets/Lucas Assets/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2479068031ecbfbf6714a658da443bd6cd1a842b0b41c1ae1a70fe402165c290
+oid sha256:7187ff34b0f1895fc8168a19e768fe457ebf46a13a689d2ead560912ce96d764
 size 1527
