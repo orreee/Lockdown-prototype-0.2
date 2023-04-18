bb968d756d50a967585a1237d64f74f725067cd9
St1erna <timekenstierna@gmail.com>
2023-04-19 00:49:13 +0200

    pls work pt2

--- Assets/Lucas Assets/Scripts/Player.cs
+++ Assets/Lucas Assets/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9b97aed3a7b277f2a06136e71b07a97321762df1aeb269efa1af2f236942059
-size 1873
+oid sha256:aeec9c98880851aaebdefa531adc56f4193df9ae33f61cc9c2505dd53e529996
+size 1876
