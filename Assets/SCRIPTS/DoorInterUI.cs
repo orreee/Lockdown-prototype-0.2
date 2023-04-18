9b93a033ca036efcdd4cfaa6d6da51a44f2c39f3
orreee <oscar.sandahl@gmail.com>
2023-04-19 01:07:06 +0200

    Revert "TextEvent at 12:00"
    This reverts commit 927f25555193efe1ce62aa04f656cf3b2ab76317.

--- Assets/SCRIPTS/DoorInterUI.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bf92736e5931427f1feae07a602f24c8a67c46e43ee99ff1f7ebb02cf86e39d6
-size 285
--- Assets/SCRIPTS/DoorTextUI.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3be6955c8a80871f2475651211de8e1a7d51ea1bf220adc9e11a3c50b8d73218
-size 478
