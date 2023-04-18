02b9ea1a4233b58c49e0b71ed07c091529c2b14e
ms0ni <lucas.jarlestrand@hotmail.com>
2023-04-18 02:11:48 +0200

    Meat eat

--- Assets/Lucas Assets/Scripts/Player.cs
+++ Assets/Lucas Assets/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7187ff34b0f1895fc8168a19e768fe457ebf46a13a689d2ead560912ce96d764
-size 1527
+oid sha256:27ced0923c44fbff01bf0be3f2af8c45e2668f94464de6edf2464f67ffd884f4
+size 1534
--- Assets/SCRIPTS/FoodShop.cs
+++ Assets/SCRIPTS/FoodShop.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:704f68c59c8183ded424c1ff63e3f02f8ced522393152d8a8696e658cd897630
-size 2178
+oid sha256:fab8bef4688d07d2202e6195c33b677d78e5607ea843323810dcb39420e86c1d
+size 2559
