927f25555193efe1ce62aa04f656cf3b2ab76317
KlintebergOscar <klintebergoscar@gmail.com>
2023-04-19 00:59:32 +0200

    TextEvent at 12:00
    TextEvent at 12:00 on day 2

---
+++ Assets/SCRIPTS/DoorEvent.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:13be95aff3a4171eceacc858344fec886789dd1073abb002997d6713acfb9657
+size 1588
---
+++ Assets/SCRIPTS/DoorInteraction.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:db6b3712d927449c9dce4c32c79c9af481001b3a9958d0e6333f7f895879fbd4
+size 317
