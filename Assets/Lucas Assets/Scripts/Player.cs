f9bd02d573ef73090d0fe745a625dce28a79f8eb
Oni <112486706+ms0ni@users.noreply.github.com>
2023-04-18 12:23:00 +0200

    GodMode
    toggle godmode G

--- Assets/Lucas Assets/Scripts/Player.cs
+++ Assets/Lucas Assets/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27ced0923c44fbff01bf0be3f2af8c45e2668f94464de6edf2464f67ffd884f4
-size 1534
+oid sha256:30988b9c74da25a2a0a2f1342855df9507ff2dcc103d9cf9010a97c5a941befc
+size 1836
