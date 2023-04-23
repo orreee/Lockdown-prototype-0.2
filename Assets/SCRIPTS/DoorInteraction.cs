c5190d15f4f08d3057565254ebd3aebc48d9ed4c
KlintebergOscar <klintebergoscar@gmail.com>
2023-04-23 16:26:43 +0200

    Tar lång tid att starta spelet
    Tar lång tid att starta spelet

--- Assets/SCRIPTS/DoorInteraction.cs
+++ Assets/SCRIPTS/DoorInteraction.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:668c82633b65d252a7987472c76720c7f7dfa1d4950220c8618dec85d6f13c32
-size 2332
+oid sha256:f04c5da9757bacc04d047c033186eda593c4a0bde7c9bce61c3586d2e5b4c351
+size 2334
