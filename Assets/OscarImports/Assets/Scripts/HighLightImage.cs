0a3430820a81b67f8a374a74b6a05f9dd4dc22a5
orreee <oscar.sandahl@gmail.com>
2023-05-08 17:25:25 +0200

    highlight pictures flash when you buy an item

--- Assets/OscarImports/Assets/Scripts/HighLightImage.cs
+++ Assets/OscarImports/Assets/Scripts/HighLightImage.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63bd20c54fd50d280870a2942115eb58dd01d9f30b58617993b29e0e4798fe2d
-size 1239
+oid sha256:a5bf06a7f2d5b287a8f6056e8b602b03d8a543d9c92023141dd6cabbef33f9c1
+size 1301
