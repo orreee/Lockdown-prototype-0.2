14a6e424e2d17f2797ec2b72aa643a1def036207
KlintebergOscar <klintebergoscar@gmail.com>
2023-05-08 21:26:33 +0200

    Pillat runt i apartmentscene
    Flyttat datorn, ökat mängden pengar man tjänar för att testing går fortare. Försökt lösa issues. Inget klart per se. Startar en ny branch

--- Assets/SCRIPTS/ComputerLogic.cs
+++ Assets/SCRIPTS/ComputerLogic.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6aad0050e1093ddbc7029650a44deb9f3d2f70a63ec20ff64a2e7eb6103d5467
-size 1465
+oid sha256:2661513c9dbf3bfa524a7ee62e8e63b09ffec523ae8e30eedb5bbe996f1a289d
+size 1466
