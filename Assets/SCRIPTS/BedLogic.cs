a5fe4eb0adda02a43e09c89cfabbc2bbd4a7c734
ms0ni <lucas.jarlestrand@hotmail.com>
2023-05-09 23:57:13 +0200

    sleepText

--- Assets/SCRIPTS/BedLogic.cs
+++ Assets/SCRIPTS/BedLogic.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6358f46adff31aa12ab6edf21e637428ff8b69ddda15ccf52c36fa8cdf26af51
-size 1010
+oid sha256:1f2899df10a0c93f1d60bdcf9d4d124f036f381a251d0b01c43f37f3e40ddb73
+size 1231
