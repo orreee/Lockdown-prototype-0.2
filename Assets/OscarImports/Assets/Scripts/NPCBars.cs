d1b6639b3d36c723b79872154787806829752d3b
Oni <112486706+ms0ni@users.noreply.github.com>
2023-05-17 17:21:23 +0200

    mother and daughter bars change when you sleep

--- Assets/OscarImports/Assets/Scripts/NPCBars.cs
+++ Assets/OscarImports/Assets/Scripts/NPCBars.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cff538d804bfc8954e4bd64d955733c0ea66314468138bac21ea91f17c963b5e
-size 3443
+oid sha256:28fa36d936cc999cda4832da3c53a727a7bb8b4c710cf659fe09a3070f21a823
+size 3450
