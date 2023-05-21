0e384f19d6d79b6d12fc06f140ba40ccb3d15f9e
orreee <oscar.sandahl@gmail.com>
2023-05-21 22:11:21 +0200

    Revert "Revert "Man kan vinna nu""
    This reverts commit 9feae9878c7e456d1c1445f0c05c0c548952605b.

---
+++ Assets/OscarImports/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e262a4bf3a33766735bc12e2802d357ad05a7fc3f49dcffe861f368616888473
+size 372
--- Assets/SCRIPTS/Timer.cs
+++ Assets/SCRIPTS/Timer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac7e27d3d525b35d1e67abf7789161f8c08fc408ef4a38943c8ade4219f2ce16
-size 1102
+oid sha256:331a1ad1d4f264e4f7d36820b266ed74ec8b0aadd2d69b8eaa973d8282354ac8
+size 1184
