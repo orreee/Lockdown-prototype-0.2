eea3279eff333a3c5bec1a21586223d369a6c186
ms0ni <lucas.jarlestrand@hotmail.com>
2023-05-31 00:44:21 +0200

    author stuff

--- Assets/OscarImports/Assets/Scripts/NPCBars.cs
+++ Assets/OscarImports/Assets/Scripts/NPCBars.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53e3eec34e1c1e82d58f9696c4c2ea81668f467adb4f47baa81a8eee84545620
-size 4235
+oid sha256:24d532544dd3ff6325607ff1f7e55281b6da5eb8378a19bb673458b3dc116262
+size 4249
