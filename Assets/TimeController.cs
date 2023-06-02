d513e13a74896dddb80676c0ab48487af63a01bb
KlintebergOscar <klintebergoscar@gmail.com>
2023-06-02 16:01:55 +0200

    Lights/doors in tutorial scene
    Lights/doors in tutorial scene

--- Assets/TimeController.cs
+++ Assets/TimeController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd0198de1a783867cecd2ed5b8bccd448645e4bfa2a25a708bfe63e475128a5a
-size 3980
+oid sha256:3f037ab4496103fddf0e7a2abdc6baae64cdc8df990632d03c1fc217acce78cb
+size 3986
