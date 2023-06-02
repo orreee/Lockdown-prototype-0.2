6ba8ac888caa2e22e27203bc916ab9b9a9c836c6
KlintebergOscar <klintebergoscar@gmail.com>
2023-06-02 18:48:54 +0200

    Dokumentation

--- Assets/SCRIPTS/DoorEvent.cs
+++ Assets/SCRIPTS/DoorEvent.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3f08af2dab0a6a048ac3a797def950d7535da5a4d9291c73528a9cca291829a
-size 915
+oid sha256:807293640dfcf5dcfbd5fe95b488515b6d9e3e6e4e61a2b9c553cf877c3ea1ae
+size 913
--- Assets/SCRIPTS/TimeController.cs
+++ Assets/SCRIPTS/TimeController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44115abfd72b6fd42b38a290d6c25f0fb068569ca5cdab2d98edc445c92b9c51
-size 3661
+oid sha256:8d6ea690c6b8c672a420430c60bf86e31e7dda27fc0327fad1445b965cc27d5b
+size 3784
